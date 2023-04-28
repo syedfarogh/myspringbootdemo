14f13bc7c6dbf92fe6f0fc0fef2b34f88f9e38ed
syedfarogh <130975292+syedfarogh@users.noreply.github.com>
2023-04-29 01:43:53 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Hi Friends Latest Devops Projects Uploaded Daily Based . LiKe & Share this video & also Subscribe";
+		return "Welcome To My Running SpringAPP Final";
 	}
 
 	public static void main(String[] args) {
